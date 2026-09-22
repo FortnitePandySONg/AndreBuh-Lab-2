ee70c24a6ab3084170046015c3570bff28fb4b10
Andre Buhain <abuhain29@setoncatholic.org>
2026-09-22 17:58:46 +0000

    game number generator

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-x = 1
-while(true):
-    print(x)
-    x = x + 1
+import random
+game_number = random.randint(1,10)
+print(game_number)
+guess = int(input("Enter a number between 1 and 10: "))
+if guess > game_number: 
+    print("Too High")
+elif guess < game_number:
+    print("Too Low")
+else:
+    print("You Win")
