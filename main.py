ee19317f11a3db8beea0e8a5190e1d896b6d80ef
Andre Buhain <abuhain29@setoncatholic.org>
2026-09-22 18:19:33 +0000

    comment with the number

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import random
-game_number = random.randint(1,100)
+game_number = random.randint(1,10)
+guess_count = 0
 #print(game_number)
 while(1):
-    guess = int(input("Enter a number between 1 and 100: "))
+    guess = int(input("Enter a number between 1 and 10: "))
+    guess_count += 1
     if guess > game_number: 
         print("Too High")
     elif guess < game_number:
         print("Too Low")
     else:
         print("You Win")
+        print(f"It took you {guess_count} guesses.")
+    if guess_count >= 5:
+        print("Wow you are really bad at this game!")
         break
